bf5acf42d55836fdc35ef53c3f89b359355a1f7e
vicfreys <vfreyssinet@gmail.com>
2019-02-03 13:06:44 +0100

    Register form added

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Socialeed</title>
+</head>
+<body>
+
+<form action="">
+    <input type="text" name="reg_fname" placeholder="First Name" required>
+    <br>
+    <input type="text" name="reg_lname" placeholder="Last Name" required>
+    <br>
+    <input type="email" name="reg_email" placeholder="Email" required>
+    <br>
+    <input type="text" name="reg_email2" placeholder="Confirm email" required>
+    <br>
+    <input type="password" name="reg_password" placeholder="Password" required>
+    <br>
+    <input type="text" name="reg_password2" placeholder="Confirm password" required>
+    <br>
+    <input type="submit" name="register_button" value="Register">
+</form>
+    
+</body>
+</html>
